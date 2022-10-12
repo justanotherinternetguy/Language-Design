b50e91b46f48238e0a0812e8513387976672a1c4
justanotherinternetguy <hendryxu1@gmail.com>
2022-10-12 01:32:09 -0400

    e

--- LispWithC/prompt.c
+++ LispWithC/prompt.c
@@ -1,18 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-// Buffer for userinput of size 2048
-static char input[2048];
+#ifdef _WIN32
+#include <string.h>
+
+static char bufer[2048];
+
+// Fake readline func
+char* readline(char* prompt) {
+    fputs(prompt, stdout);
+    fgets(buffer, 2048, stdin);
+    char* cpy = malloc(strlen(buffer) + 1);
+    strcpy(cpy, buffer);
+    cpy[strlen(cpy) - 1] = '\0';
+    return cpy
+}
+
+// Fake add_history func
+void add_history(char* unused) {}
+
+#else
+#include <readline/readline.h>
+#include <readline/history.h>
+#endif
 
 int main(int argc, char** argv) {
     while (1) {
-        // print prompt
-        fputs("LispCy >> ", stdout);
-
-        // Readline into input buffer
-        fgets(input, 2048, stdin);
-
-        // print input back to user
-        printf("you said %s", input);
+        char* input = readline("LispCy >> ");
+        add_history(input);
+        
+        printf("you typed %s\n", input);
+        free(input);
     }
     return 0;
 }
